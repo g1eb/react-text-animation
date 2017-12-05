cc9fcb51c21e8c09c46e333a98fcfcd435fc396e
g1eb <mail@g1eb.com>
2017-12-05 11:22:47 +0100

    Add initial webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+var webpack = require('webpack');
+var path = require('path');
+var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+
+module.exports = {
+
+  entry: {
+    'text-animation': './src/index.js',
+    'text-animation.min': './src/index.js'
+  },
+
+  externals: {
+    'react': {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react',
+      umd: 'react'
+    },
+    'react-dom': {
+      root: 'ReactDOM',
+      commonjs2: 'react-dom',
+      commonjs: 'react-dom',
+      amd: 'react-dom',
+      umd: 'react-dom'
+    },
+  },
+
+  output: {
+    filename: '[name].js',
+    chunkFilename: '[id].chunk.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    libraryTarget: 'umd',
+    library: 'TextAnimation'
+  },
+
+  plugins: [
+    new UglifyJsPlugin({
+      include: /\.min\.js$/,
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+
+};
